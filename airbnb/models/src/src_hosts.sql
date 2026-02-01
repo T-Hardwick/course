777d448beffb4689d58368df9e8cf0fa4b1c7c76
tylor <thardwick@eou.edu>
2026-02-01 10:56:28 -0800

    second change

---
+++ airbnb/models/src/src_hosts.sql
@@ -0,0 +1,16 @@
+WITH raw_hosts AS (
+    SELECT
+        *
+    FROM
+        AIRBNB.RAW.RAW_HOSTS
+)
+SELECT
+    ID AS host_id,
+    name AS host_name,
+    is_superhost,
+    created_at,
+    updated_at
+    
+    
+
+FROM raw_hosts
